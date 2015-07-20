8865b473ab3462920eeea40e55c8bfd08f1713ba
Elisa <elisa@elisapc>
2015-07-20 19:54:59 +0200

    added stylesheet

--- djangle/settings.py
+++ djangle/settings.py
@@ -101,6 +101,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    'djangle/static',
+    'forum/static',
+)
+
 
 AUTH_USER_MODEL = 'forum.User'
 
