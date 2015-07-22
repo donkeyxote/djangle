cad701f102053a70d41f8db65f740fbc5389320f
donkeyxote <donkeyxote@users.noreply.github.com>
2015-07-22 18:20:42 +0200

    removed circular reference between post and thread, added creation method for post, added some defaults

--- forum/admin.py
+++ forum/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Post, Thread, Board
 # Register your models here.
 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
-
 admin.site.register(User, UserAdmin)
+admin.site.register(Post)
+admin.site.register(Thread)
+admin.site.register(Board)
--- forum/models.py
+++ forum/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 # Create your models here.
 
 
 class Board(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=10)
+
+    def get_latest(self, num):
+        return self.thread_set.order_by('-last_post()')[:num]
 
     def __str__(self):
         return self.code
@@ -14,7 +18,7 @@ class Board(models.Model):
 class User(AbstractUser):
     models.EmailField.unique = True
     rep = models.IntegerField(default=0)
-    avatar = models.ImageField(default='static/djangle/images/Djangle_logo.png')
+    avatar = models.ImageField(default='/static/djangle/images/Djangle_logo.png')
     posts = models.PositiveIntegerField(default=0)
     threads = models.PositiveIntegerField(default=0)
 
@@ -24,36 +28,48 @@ class Post(models.Model):
     pub_date = models.DateTimeField('publication date')
     in_thread = models.ForeignKey('Thread')
     author = models.ForeignKey(User)
-    pos_votes = models.PositiveIntegerField()
-    neg_votes = models.PositiveIntegerField()
+    pos_votes = models.PositiveIntegerField(default=0)
+    neg_votes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.message[:50]
 
+    @classmethod
+    def create(cls, message, thread, author):
+        pub_date = timezone.now()
+        post = cls(message=message, pub_date=pub_date, in_thread=thread, author=author)
+        post.save()
+        if post.in_thread.first_post is None:
+            post.in_thread.first_post = post
+        post.in_thread.save()
+        return post
+
 
 class Thread(models.Model):
     title = models.CharField(max_length=200)
-    first_post = models.ForeignKey(Post, related_name='thread_message')
+    first_post = models.ForeignKey(Post, related_name='thread_message', blank=True, null=True, default=None)
     open = models.BooleanField
-    close_date = models.DateTimeField('closed on')
-    closer = models.ForeignKey(User)
-    tag1 = models.CharField(max_length=50)
-    tag2 = models.CharField(max_length=50)
-    tag3 = models.CharField(max_length=50)
-    last_post = models.ForeignKey(Post)
+    close_date = models.DateTimeField('closed on', blank=True, null=True, default=None)
+    closer = models.ForeignKey(User, blank=True, null=True, default=None)
+    tag1 = models.CharField(max_length=50, blank=True, null=True, default=None)
+    tag2 = models.CharField(max_length=50, blank=True, null=True, default=None)
+    tag3 = models.CharField(max_length=50, blank=True, null=True, default=None)
     board = models.ForeignKey(Board)
 
     def __str__(self):
         return self.title[:50]
 
+    def last_post(self):
+        return self.post_set.last()
+
 
 class Subscription(models.Model):
     thread = models.ForeignKey(Thread)
     user = models.ForeignKey(User)
-    async = models.BooleanField()
-    sync_interval = models.DurationField('sync interval')
-    last_sync = models.DateTimeField('last syncronization')
-    active = models.BooleanField()
+    async = models.BooleanField(default=True)
+    sync_interval = models.DurationField('sync interval', blank=True, null=True, default=None)
+    last_sync = models.DateTimeField('last syncronization', blank=True, null=True, default=None)
+    active = models.BooleanField(default=True)
 
 
 class Vote(models.Model):
@@ -64,7 +80,7 @@ class Vote(models.Model):
 
 class Ban(models.Model):
     user = models.ForeignKey(User)
-    start = models.DateTimeField('start on')
+    start = models.DateTimeField('start on', default=timezone.now)
     duration = models.DurationField('duration')
     banner = models.ForeignKey(User, related_name='banner')
     reason = models.CharField(max_length=50)
